51080748d1c65156922b18a696bf0d4d08a71bf1
Benjamin Braithwaite <478171+bgbraithwaite@users.noreply.github.com>
2021-07-14 12:19:06 -0400

    Updated Basic JavaScript exercises

---
+++ javascript-algorithms-and-data-structures-certification/1-basic-javascript/046-shopping-list.js
@@ -0,0 +1,1 @@
+var myList = [["Pony", 1000], ["Cheetos", 3], ["Donut", 1], ["Coffee", 2], ["Book", 10]];
---
+++ javascript-algorithms-and-data-structures-certification/1-basic-javascript/048-passing-values-to-functions-with-arguments.js
@@ -0,0 +1,5 @@
+function functionWithArgs(i, j) {
+  console.log(i + j);
+}
+
+functionWithArgs(30, 10);
