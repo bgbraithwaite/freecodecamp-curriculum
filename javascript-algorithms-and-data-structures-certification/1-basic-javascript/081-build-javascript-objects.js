2ec38f064adf387aea1151b44d44684c87726fad
Benjamin Braithwaite <478171+bgbraithwaite@users.noreply.github.com>
2021-08-09 13:44:15 -0400

    Updated Basic JavaScript exercises

---
+++ javascript-algorithms-and-data-structures-certification/1-basic-javascript/081-build-javascript-objects.js
@@ -0,0 +1,10 @@
+var myDog = {
+// Only change code below this line
+
+  name: "Charlie",
+  legs: 4,
+  tails: 1,
+  friends: ["Barry", "Terry", "Larry"]
+
+// Only change code above this line
+};
---
+++ javascript-algorithms-and-data-structures-certification/1-basic-javascript/090-manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Radiohead",
+    "title": "MiniDiscs [Hacked]",
+    "release_year": 2019,
+    "formats": [
+      "Digital (Lossless)",
+      "Digital (MP3)"
+    ]
+  }
+];
